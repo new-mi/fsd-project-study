3700d5c4d401c0a275e3631026834c4d7e297db0
Mikhail Vagin <mvagin@bk.ru>
2025-06-01 22:27:19 +0300

    Изменен экспорт компонента App на экспорт по умолчанию и добавлена проверка наличия корневого элемента в main.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 export const App = () => {
   return <div>Application</div>;
 };
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+} else {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
