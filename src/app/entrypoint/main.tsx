af3fda58a604e1917a801814491e99b11aa1de4c
Mikhail Vagin <mvagin@bk.ru>
2025-06-02 20:11:58 +0300

    Добавлены новые зависимости для работы с TypeScript и улучшения конфигурации проекта. Обновлены пути импорта в файлах приложения для использования алиасов. Удалены неиспользуемые файлы index.ts в папках pages и shared.

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import "../styles/index.css";
-import { routes } from "../routes";
+import { routes } from "@/app/routes";
+import "@/app/styles/index.css";
 
 const rootElement = document.getElementById("root");
 
--- src/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export { ROUTES } from "./routes";
-export { CONFIG, type IEnvConfig } from "./config";
-export { http } from "./api";
-export * from "./ui";
